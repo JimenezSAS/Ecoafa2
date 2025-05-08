9f706c76524f4259b9c3af19cf22baa8afc10c5c
Dainover <jimenezelkin02@gmail.com>
2025-05-08 10:14:56 -0500

    Header

--- ecoafa2/src/Components/Header/Header.jsx
+++ ecoafa2/src/Components/Header/Header.jsx
@@ -1,45 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/logo/logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo/logo.svg";
 const Header = () => {
   return (
-    <div className='w-full h-full flex items-center justify-center'>
-      <header className='p-0 gap-0 flex items-center justify-between w-full h-full'>
-        <div className='flex items-center'>
-          <img src={Logo} alt="Logo" className='h-20' />
+    <div className="w-full h-full flex items-center justify-center">
+      <header className="p-0 gap-0 flex items-center justify-between w-full h-full">
+        <div className="flex items-center">
+          <img src={Logo} alt="Logo" className="h-20 flex justify-center items-center" />
         </div>
-        <div
-          className='flex gap-15 h-10 text-[20px] text-[#0092B2]'
-          
-        >
-          <Link
-            to="/"
-            className="hover:text-[#93B611] hover:text-[22px] transition-all duration-300"
-          >
+        <div className="flex gap-15 h-10 text-[20px] text-[#0092B2]">
+
+          <Link to="/" className="hover:text-[#93B611] hover:bg-gray-200 flex  rounded-[10px] hover:text-[22px] transition-all duration-300">
             Home
           </Link>
-          <Link
-            to="/about"
-            className="hover:text-[#93B611] hover:text-[22px] transition-all duration-300"
-          >
+
+          <Link to="/about" className="hover:text-[#93B611] hover:bg-gray-200 flex  rounded-[10px] hover:text-[22px] transition-all duration-300">
             About
           </Link>
-          <Link
-            to="/contact"
-            className="hover:text-[#93B611] hover:text-[22px] transition-all duration-300"
-          >
+
+          <Link to="/contact"  className="hover:text-[#93B611] hover:bg-gray-200 flex  rounded-[10px] hover:text-[22px] transition-all duration-300">
             Contact
           </Link>
-          <Link
-            to="/services"
-            className="hover:text-[#93B611] hover:text-[22px] transition-all duration-300"
-          >
+
+          <Link to="/services" className="hover:text-[#93B611] hover:bg-gray-200 flex  rounded-[10px] hover:text-[22px] transition-all duration-300" >
             Services
           </Link>
+
         </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
